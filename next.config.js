7c24c68b8f2cae3bd0ccfdeb28d8c53b96a88900
iitian_adi05tya <131003170+iitian-adi05tya@users.noreply.github.com>
2025-12-19 16:21:05 +0530

    Add files via upload

---
+++ next.config.js
@@ -0,0 +1,9 @@
+module.exports = {
+    reactStrictMode: true,
+    env: {
+      OPENAI_API_KEY: process.env.OPENAI_API_KEY,
+      YOUTUBE_API_KEY: process.env.YOUTUBE_API_KEY,
+      GOOGLE_API_KEY: process.env.GOOGLE_API_KEY,
+      GOOGLE_CX: process.env.GOOGLE_CX,
+    }
+  }
